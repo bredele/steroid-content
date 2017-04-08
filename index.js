976a8986710a03daf1442cb0e8f107f9d3fe1f4f
bredele <olivier.wietrich@gmail.com>
2017-04-08 10:44:49 -0600

    select pseudo element

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
+/**
+ * Dependencies
+ */
 
+const engine = require('trumpet')
+const through = require('through')
+
+/**
+ * Expose 'content' to be used primarly
+ * with sa declarative syntax
+ * @see https://github.com/bredele/steroid-hook
+ */
 
 module.exports = content
+
+
+/**
+ *
+ */
 
 function content() {
 
 }
 
 
-content.query = function() {
-  
+content.query = function(selector, source) {
+  const query = engine()
+  const dest = through(
+    function(data) {
+      this.queue(data)
+    },
+    function() {
+      this.queue(null)
+    }
+  )
+  query.selectAll(selector, function (span) {
+    span.createReadStream({
+      outer: true
+    }).pipe(dest)
+  })
+  source.pipe(query)
+  return dest
 }
